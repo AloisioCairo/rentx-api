5988ca8e01eca512f01503b6bd75058a041afcb0
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-07-30 07:32:07 -0300

    Implementado somente para teste a obrigatoriedade do token do usuário

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,5 +1,6 @@
 // 1.	Capitulo 2 > Iniciando a API > Continuação da aplicação > Criando o service de especificação e separando em modulos
 import { Router } from "express";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
@@ -8,6 +9,7 @@ const specificationsRoutes = Router();
 // Capítulo 3 > Continuando a aplicação > Trabalhando com Banco de Dados > Refatorando as especificações
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticated)
 specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes }
