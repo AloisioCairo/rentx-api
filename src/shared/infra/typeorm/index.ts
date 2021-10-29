7dd7eeff7812575fe748fbe05f118dab2ddba858
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-10-28 22:36:32 -0300

    feat: Alterado a forma de criar a conexão com o banco de dados

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,14 +1,25 @@
 // Capítulo 3 > Continuando a aplicação > Trabalhando com Banco de Dados > Instalando o TypeORM
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-interface IOptions {
-    host: string;
+// interface IOptions {
+//     host: string;
+// }
+
+// getConnectionOptions().then(options => {
+//     const newOptions = options as IOptions;
+//     newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//     createConnection({
+//         ...options,
+//     });
+// });
+
+// Capítulo 4 > Testes e regras de negócio > Carros > Criando seed de usuário
+export default async (host = "database"): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            host,
+        })
+    );
 }
-
-getConnectionOptions().then(options => {
-    const newOptions = options as IOptions;
-    newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-    createConnection({
-        ...options,
-    });
-});
