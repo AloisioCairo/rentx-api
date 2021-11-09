7823635f32d82ca3e779e4aca97b7ccd698447c2
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-11-09 07:29:13 -0300

    feat: Implmentado a migração da tabela de especificação de carros

---
+++ src/shared/infra/typeorm/migrations/1636138476110-CreateSpecificationsCars.ts
@@ -0,0 +1,66 @@
+// Capítulo 4 > Testes e regras de negócio > Carros > Criando migrations especificação de carros (Many to Many)
+
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
+
+export class CreateSpecificationsCars1636138476110 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "specifications_cars",
+                columns: [
+                    {
+                        name: "car_id",
+                        type: "uuid",
+                    },
+                    {
+                        name: "specification_id",
+                        type: "uuid",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            })
+        );
+
+        await queryRunner.createForeignKey(
+            "specifications_cars",
+            new TableForeignKey({
+                name: "FKSpecificationCar",
+                referencedTableName: "specifications",
+                referencedColumnNames: ["id"],
+                columnNames: ["specification_id"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL",
+            })
+        );
+
+        await queryRunner.createForeignKey(
+            "specifications_cars",
+            new TableForeignKey({
+                name: "FKCarSpecification",
+                referencedTableName: "cars",
+                referencedColumnNames: ["id"],
+                columnNames: ["car_id"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL",
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey(
+            "specifications_cars", "FKSpecificationCar"
+        );
+
+        await queryRunner.dropForeignKey(
+            "specifications_cars", "FKCarSpecification"
+        );
+
+        await queryRunner.dropTable("specifications_cars");
+    }
+
+}
