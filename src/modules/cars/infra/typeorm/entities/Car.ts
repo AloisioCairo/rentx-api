fbb1ba870151e7f9f0f76ea5b9b977c7ecb3c94a
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-11-09 07:31:05 -0300

    feat: Implementado a regra de muitos para muitos (carros > especificação)

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,8 +1,9 @@
 //	Capitulo 4 > Testes e regras de negócio > Carros > TDD na prática
 //	Capitulo 4 > Testes e regras de negócio > Carros > Estruturando a entidade de carros   - Inclusão do typeorm
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 import { Category } from "./Category";
+import { Specification } from "./Specification";
 
 @Entity("cars")
 class Car {
@@ -16,7 +17,7 @@ class Car {
     description: string;
 
     @Column()
-    daily_rate: boolean;
+    daily_rate: number;
 
     @Column()
     available: boolean;
@@ -35,7 +36,16 @@ class Car {
     category: Category;
 
     @Column()
-    category_id: String;
+    category_id: string;
+
+    // Capítulo 4 > Testes e regras de negócio > Carros > Caso de uso do cadastro de especificação para carro
+    @ManyToMany(() => Specification)
+    @JoinTable({
+        name: "specifications_cars",
+        joinColumns: [{ name: "car_id" }],
+        inverseJoinColumns: [{ name: "specification_id" }]
+    })
+    specifications: Specification[];
 
     @CreateDateColumn()
     created_at: Date;
