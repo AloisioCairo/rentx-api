78c7a8fb90d160e225031e71165c62a4804f16df
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-11-09 07:42:32 -0300

    feat: Implementado a busca de todas as especificações de um carro a partir do ID

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,5 +1,6 @@
 // 1. Capitulo 2 > Iniciando a API > Continuação da aplicação > Criando repositório de especificação
-import { Specification } from "../entities/Specification";
+
+import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
 
 interface ICreateSpecificationDTO {
     name: string;
@@ -7,9 +8,9 @@ interface ICreateSpecificationDTO {
 }
 
 interface ISpecificationsRepository {
-    create({ name, description }: ICreateSpecificationDTO): Promise<void>;
-
+    create({ name, description }: ICreateSpecificationDTO): Promise<Specification>;
     findByName(name: string): Promise<Specification>;
+    findByIds(ids: string[]): Promise<Specification[]>; // Capítulo 4 > Testes e regras de negócio > Carros > Continuação dos CreateCarSpecificationUseCase
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
