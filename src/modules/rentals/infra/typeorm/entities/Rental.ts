83e2a15e8f0a925ed61d103685a7ff474bc6a7ed
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-11-15 12:49:38 -0300

    feat: Impletado a injeção de dependências nos campos da entidade rentals

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,23 +1,34 @@
 //  Capítulo 4 > Testes e regras de negócio > Aluguel > Criando os testes do aluguel
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("rentals")
 class Rental {
+    @PrimaryColumn()
     id: string;
 
+    @Column()
     car_id: string;
 
+    @Column()
     user_id: string;
 
+    @Column()
     start_date: Date;
 
-    end_data: Date;
+    @Column()
+    end_date: Date;
 
-    expect_return_date: Date;
+    @Column()
+    expected_return_date: Date;
 
+    @Column()
     total: number;
 
+    @CreateDateColumn()
     created_at: Date;
 
+    @UpdateDateColumn()
     updated_at: Date;
 
     constructor() {
