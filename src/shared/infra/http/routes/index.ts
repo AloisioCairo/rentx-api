c9ea30a6da412f17f477e7a19d9ba5269d789a0f
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-11-15 12:53:57 -0300

    feat: Adicionado a rota da entidade rental

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,12 @@
 // 1.	Capitulo 2 > Iniciando a API > Continuação da aplicação > Refatorando as rotas
 import { Router } from "express";
 
-import { categoriesRoutes } from "./categories.routes"
-import { specificationsRoutes } from "./specification.routes"
-import { usersRoutes } from "./users.routes"
-import { authenticateRoutes } from "./authenticate.routes"
-import { carsRoutes } from "./cars.routes"
+import { categoriesRoutes } from "./categories.routes";
+import { specificationsRoutes } from "./specification.routes";
+import { usersRoutes } from "./users.routes";
+import { authenticateRoutes } from "./authenticate.routes";
+import { carsRoutes } from "./cars.routes";
+import { rentalRoutes } from "./rentals.routes";
 
 const router = Router();
 
@@ -13,6 +14,7 @@ router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
 router.use('/cars', carsRoutes);
+router.use('/rentals', rentalRoutes);
 router.use(authenticateRoutes);
 
 export { router }
