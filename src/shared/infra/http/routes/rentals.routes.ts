608a61442c534fa640ef8f83465505740c4d43c7
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-11-15 12:53:18 -0300

    Implementado a rota da entidade rental

---
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -0,0 +1,13 @@
+// Capítulo 4 > Testes e regras de negócio > Aluguel > Criando controller
+import { Router } from "express";
+
+import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
+const rentalRoutes = Router();
+
+const createRentalController = new CreateRentalController();
+
+rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+
+export { rentalRoutes }
