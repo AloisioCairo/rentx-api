20bc6753f66ddfce81062ae84198fe42093ccb63
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-11-18 07:08:27 -0300

    feat: Criado a variável para identificar em qual banco de dados (teste ou produção) o app irá conectar e implementado teste de integração para a criação e listam de categorias

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,73 @@
+// Capítulo 4 > Testes e regras de negócio > Testes de integração > Configurando supertest
+import request from "supertest";
+import { Connection } from "typeorm";
+import { hash } from "bcryptjs";
+import { v4 as uuid } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+
+// Capítulo 4 > Testes e regras de negócio > Testes de integração > Criando o primeiro teste de integração
+// Capítulo 4 > Testes e regras de negócio > Testes de integração > Continuando o teste de integração
+let connection: Connection;
+
+describe("Create Category Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const id = uuid();
+        const password = await hash("admin", 8);
+
+        await connection.query(`INSERT INTO users (id, name, email, password, "isAdmin", created_at, driver_license)
+    VALUES ('${id}', 'admin', 'admin@rentex.com.br', '${password}', true, 'now()', 'XXXXXX')`);
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    })
+
+    it("should be able to create a new category", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentex.com.br",
+            password: "admin",
+        });
+
+        const { token } = responseToken.body;
+
+        const response = await request(app)
+            .post("/categories")
+            .send({
+                name: "Category Supertest",
+                description: "Category Supertest",
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(201);
+    })
+
+    // Capítulo 4 > Testes e regras de negócio > Testes de integração > Criando teste de listagem de categorias
+    it("should not be able to create a new category with name exists", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentex.com.br",
+            password: "admin",
+        });
+
+        const { token } = responseToken.body;
+
+        const response = await request(app)
+            .post("/categories")
+            .send({
+                name: "Category Supertest",
+                description: "Category Supertest",
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(400);
+    })
+})
