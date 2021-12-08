8cbd601ab2a96059b4b5e302585f4ca1342a7114
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-12-07 21:32:05 -0300

    feat: Implementado a visualização dos dados do carro

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,11 +1,17 @@
 //  Capítulo 4 > Testes e regras de negócio > Aluguel > Criando os testes do aluguel
-import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
 @Entity("rentals")
 class Rental {
     @PrimaryColumn()
     id: string;
+
+    // Capítulo 5 > Trabalhanco com refresh_token e e-mail > Carro > Refatorando a listagem de aluguel do usuário
+    @ManyToOne(() => Car)
+    @JoinColumn({ name: "car_id" })
+    car: Car;
 
     @Column()
     car_id: string;
