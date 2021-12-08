14a003dfa24fd8832c22eebf4b9d8041ad6e15ec
ALOISIO SAMPAIO CAIRO <aloisiocairo@gmail.com>
2021-12-07 21:27:13 -0300

    feat: Implementado a busca de locações pelo id e pelo código do usuário

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -6,6 +6,8 @@ interface IRentalsRepository {
     findOpenRentalByCar(car_id: string): Promise<Rental>;
     findOpenRentalByUser(user_id: string): Promise<Rental>;
     create(data: ICreateRentalDTO): Promise<Rental>; //  Capítulo 4 > Testes e regras de negócio > Aluguel > Continuação do cadastro de aluguel
+    findById(id: string): Promise<Rental>; // Capítulo 5 > Trabalhanco com refresh_token e e-mail > Carro > Caso de uso de devolução de carro
+    findByUser(user_id: string): Promise<Rental[]>; // Capítulo 5 > Trabalhanco com refresh_token e e-mail > Carro > Listagem de alugueis de usuário
 }
 
 export { IRentalsRepository }
